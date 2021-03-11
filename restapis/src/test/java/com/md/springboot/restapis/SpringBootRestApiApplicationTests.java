08b8af3bb57ed260efba70cbf49e433ebbbe08f8
Milind <milinddholakia0@gmail.com>
2021-03-11 21:19:43 +0530

    StartFile

---
+++ restapis/src/test/java/com/md/springboot/restapis/SpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.md.springboot.restapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
